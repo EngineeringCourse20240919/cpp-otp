24fcb9a9ac03a188ed4890a5c678d96652dd6768
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2024-09-17 11:19:52 +0800

    :sparkles: refactor tests

--- test/AuthenticationServiceTest.cc
+++ test/AuthenticationServiceTest.cc
@@ -19,56 +19,62 @@ public:
 
 namespace {
 
-    TEST(AuthenticationService, IsValid) {
-        GStubProfileDao* stubProfileDao = new GStubProfileDao();
-        ON_CALL(*stubProfileDao, getPassword(::testing::_)).WillByDefault(::testing::Return("91"));
-        GStubRsaTokenDao* stubRsaTokenDao = new GStubRsaTokenDao();
-        ON_CALL(*stubRsaTokenDao, getRandom(::testing::_)).WillByDefault(::testing::Return("000000"));
-        GMockLogger* mockLogger = new GMockLogger();
-        AuthenticationService target = AuthenticationService(stubProfileDao, stubRsaTokenDao, mockLogger);
+class AuthenticationServiceTest : public ::testing::Test {
+protected:
+    GStubProfileDao* stubProfileDao;
+    GStubRsaTokenDao* stubRsaTokenDao;
+    GMockLogger *mockLogger;
+    AuthenticationService* target;
 
-        bool actual = target.isValid("joey", "91000000");
+    void SetUp() override {
+        stubProfileDao = new GStubProfileDao();
+        stubRsaTokenDao = new GStubRsaTokenDao();
+        mockLogger = new GMockLogger();
+        target = new AuthenticationService(stubProfileDao, stubRsaTokenDao, mockLogger);
+    }
+
+    void TearDown() override {
+        delete stubProfileDao;
+        delete stubRsaTokenDao;
+        delete mockLogger;
+        delete target;
+    }
+
+    void givenPassword(const std::string& password) {
+        ON_CALL(*stubProfileDao, getPassword(::testing::_)).WillByDefault(::testing::Return(password));
+    }
+
+    void givenRandom(const std::string& random) {
+        ON_CALL(*stubRsaTokenDao, getRandom(::testing::_)).WillByDefault(::testing::Return(random));
+    }
+};
+
+    TEST_F(AuthenticationServiceTest, IsValid) {
+        givenPassword("91");
+        givenRandom("000000");
+
+        bool actual = target->isValid("joey", "91000000");
 
         ASSERT_TRUE(actual);
-
-        delete stubProfileDao;
-        delete stubRsaTokenDao;
-        delete mockLogger;
     }
 
-    TEST(AuthenticationService, IsNotValid) {
-        GStubProfileDao* stubProfileDao = new GStubProfileDao();
-        ON_CALL(*stubProfileDao, getPassword(::testing::_)).WillByDefault(::testing::Return("91"));
-        GStubRsaTokenDao* stubRsaTokenDao = new GStubRsaTokenDao();
-        ON_CALL(*stubRsaTokenDao, getRandom(::testing::_)).WillByDefault(::testing::Return("123456"));
-        GMockLogger* mockLogger = new GMockLogger();
-        AuthenticationService target = AuthenticationService(stubProfileDao, stubRsaTokenDao, mockLogger);
+    TEST_F(AuthenticationServiceTest, IsNotValid) {
+        givenPassword("91");
+        givenRandom("123456");
 
-        bool actual = target.isValid("joey", "91000000");
+        bool actual = target->isValid("joey", "91000000");
 
         ASSERT_FALSE(actual);
-
-        delete stubProfileDao;
-        delete stubRsaTokenDao;
-        delete mockLogger;
     }
 
-    TEST(AuthenticationService, LogMessageWhenNotValid) {
-        GStubProfileDao* stubProfileDao = new GStubProfileDao();
-        ON_CALL(*stubProfileDao, getPassword(::testing::_)).WillByDefault(::testing::Return("91"));
-        GStubRsaTokenDao* stubRsaTokenDao = new GStubRsaTokenDao();
-        ON_CALL(*stubRsaTokenDao, getRandom(::testing::_)).WillByDefault(::testing::Return("123456"));
-        GMockLogger* mockLogger = new GMockLogger();
+    TEST_F(AuthenticationServiceTest, LogMessageWhenNotValid) {
+        givenPassword("91");
+        givenRandom("123456");
         EXPECT_CALL(*mockLogger, log(::testing::Eq("login failed with userName: joey"))).Times(1);
-        AuthenticationService target = AuthenticationService(stubProfileDao, stubRsaTokenDao, mockLogger);
 
-        bool actual = target.isValid("joey", "91000000");
+        bool actual = target->isValid("joey", "91000000");
 
         ASSERT_FALSE(actual);
-
-        delete stubProfileDao;
-        delete stubRsaTokenDao;
-        delete mockLogger;
     }
 
 }
