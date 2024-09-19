6036506f69c1697fba4d7acb8a8016b31c494067
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2024-09-19 21:21:13 +0800

    :white_check_mark: add more tests

--- test/QueryBudgetTest.cc
+++ test/QueryBudgetTest.cc
@@ -6,33 +6,27 @@ class StubBudgetRepo : public BudgetRepo {
 public:
     virtual std::vector<Budget> findAll() override
     {
-        std::vector<Budget> budgets;
-
-	    budgets.push_back(Budget(31, date::year_month(date::year{ 2023 }, date::month{ 7 })));
-		budgets.push_back(Budget(31, date::year_month(date::year{ 2023 }, date::month{ 8 })));
-		budgets.push_back(Budget(30, date::year_month(date::year{ 2023 }, date::month{ 9 })));
-		budgets.push_back(Budget(31, date::year_month(date::year{ 2023 }, date::month{ 10 })));
-		budgets.push_back(Budget(30, date::year_month(date::year{ 2023 }, date::month{ 11 })));
-		budgets.push_back(Budget(31, date::year_month(date::year{ 2023 }, date::month{ 12 })));
-		budgets.push_back(Budget(31, date::year_month(date::year{ 2024 }, date::month{ 1 })));
-		budgets.push_back(Budget(29, date::year_month(date::year{ 2024 }, date::month{ 2 })));
-		budgets.push_back(Budget(31, date::year_month(date::year{ 2024 }, date::month{ 3 })));
-		budgets.push_back(Budget(30, date::year_month(date::year{ 2024 }, date::month{ 4 })));
-		budgets.push_back(Budget(31, date::year_month(date::year{ 2024 }, date::month{ 5 })));
-		budgets.push_back(Budget(30, date::year_month(date::year{ 2024 }, date::month{ 6 })));
-		budgets.push_back(Budget(31, date::year_month(date::year{ 2024 }, date::month{ 7 })));
-		budgets.push_back(Budget(31, date::year_month(date::year{ 2024 }, date::month{ 8 })));
-		budgets.push_back(Budget(30, date::year_month(date::year{ 2024 }, date::month{ 9 })));
-		budgets.push_back(Budget(31, date::year_month(date::year{ 2024 }, date::month{ 10 })));
-		budgets.push_back(Budget(30, date::year_month(date::year{ 2024 }, date::month{ 11 })));
-		budgets.push_back(Budget(31, date::year_month(date::year{ 2024 }, date::month{ 12 })));
 		return budgets;
 	}
+    std::vector<Budget> budgets;
 };
 
 namespace {
+    TEST(QueryBudgetTest, dailyAmount) {
+        StubBudgetRepo* budgetRepo = new StubBudgetRepo();
+        budgetRepo->budgets.push_back(Budget(300, year(2023) / month(9)));
+        QueryBudget *queryBudget = new QueryBudget(budgetRepo);
+        int amount = queryBudget->Calculate(date::year_month_day(date::year{2023}, date::month{9}, date::day{10}),
+                                            date::year_month_day(date::year{2023}, date::month{9},date::day{16}));
+        ASSERT_EQ(amount,7*10);
+
+        delete budgetRepo;
+        delete queryBudget;
+    }
+
     TEST(QueryBudgetTest, calculateSameMonth) {
         StubBudgetRepo* budgetRepo = new StubBudgetRepo();
+        budgetRepo->budgets.push_back(Budget(30, year(2023) / month(9)));
         QueryBudget *queryBudget = new QueryBudget(budgetRepo);
         int amount = queryBudget->Calculate(date::year_month_day(date::year{2023}, date::month{9}, date::day{10}),
         date::year_month_day(date::year{2023}, date::month{9},date::day{16}));
@@ -42,8 +36,35 @@ namespace {
         delete queryBudget;
     }
 
-TEST(QueryBudgetTest, calculateDiffMonth) {
+    TEST(QueryBudgetTest, startBeforeBudgetStart) {
         StubBudgetRepo* budgetRepo = new StubBudgetRepo();
+        budgetRepo->budgets.push_back(Budget(30, year(2023) / month(9)));
+        QueryBudget *queryBudget = new QueryBudget(budgetRepo);
+        int amount = queryBudget->Calculate(date::year_month_day(date::year{2023}, date::month{8}, date::day{10}),
+                                            date::year_month_day(date::year{2023}, date::month{9},date::day{16}));
+        ASSERT_EQ(amount,16);
+
+        delete budgetRepo;
+        delete queryBudget;
+    }
+
+    TEST(QueryBudgetTest, endAfterBudgetEnd) {
+        StubBudgetRepo* budgetRepo = new StubBudgetRepo();
+        budgetRepo->budgets.push_back(Budget(30, year(2023) / month(9)));
+        QueryBudget *queryBudget = new QueryBudget(budgetRepo);
+        int amount = queryBudget->Calculate(date::year_month_day(date::year{2023}, date::month{9}, date::day{10}),
+                                            date::year_month_day(date::year{2023}, date::month{10},date::day{16}));
+        ASSERT_EQ(amount,21);
+
+        delete budgetRepo;
+        delete queryBudget;
+    }
+
+    TEST(QueryBudgetTest, calculateDiffMonth) {
+        StubBudgetRepo* budgetRepo = new StubBudgetRepo();
+        budgetRepo->budgets.push_back(Budget(31, year(2023) / month(7)));
+        budgetRepo->budgets.push_back(Budget(31, year(2023) / month(8)));
+        budgetRepo->budgets.push_back(Budget(30, year(2023) / month(9)));
         QueryBudget *queryBudget = new QueryBudget(budgetRepo);
         int amount = queryBudget->Calculate(date::year_month_day(date::year{2023}, date::month{7}, date::day{10}),
         date::year_month_day(date::year{2023}, date::month{9},date::day{16}));
@@ -53,8 +74,23 @@ TEST(QueryBudgetTest, calculateDiffMonth) {
         delete queryBudget;
     }
 
-TEST(QueryBudgetTest, calculateDiffYear) {
+    TEST(QueryBudgetTest, missingMiddleBudget) {
         StubBudgetRepo* budgetRepo = new StubBudgetRepo();
+        budgetRepo->budgets.push_back(Budget(31, year(2023) / month(7)));
+        budgetRepo->budgets.push_back(Budget(30, year(2023) / month(9)));
+        QueryBudget *queryBudget = new QueryBudget(budgetRepo);
+        int amount = queryBudget->Calculate(date::year_month_day(date::year{2023}, date::month{7}, date::day{10}),
+                                            date::year_month_day(date::year{2023}, date::month{9},date::day{16}));
+        ASSERT_EQ(amount,22 + 16);
+
+        delete budgetRepo;
+        delete queryBudget;
+    }
+
+    TEST(QueryBudgetTest, calculateDiffYear) {
+        StubBudgetRepo* budgetRepo = new StubBudgetRepo();
+        budgetRepo->budgets.push_back(Budget(31, year(2023) / month(12)));
+        budgetRepo->budgets.push_back(Budget(31,year(2024) / month(1)));
         QueryBudget *queryBudget = new QueryBudget(budgetRepo);
         int amount = queryBudget->Calculate(date::year_month_day(date::year{2023}, date::month{12}, date::day{10}),
         date::year_month_day(date::year{2024}, date::month{1},date::day{16}));
@@ -67,6 +103,7 @@ TEST(QueryBudgetTest, calculateDiffYear) {
     
 TEST(QueryBudgetTest, calculateBefore) {
         StubBudgetRepo* budgetRepo = new StubBudgetRepo();
+        budgetRepo->budgets.push_back(Budget(30, year(2023) / month(9)));
         QueryBudget *queryBudget = new QueryBudget(budgetRepo);
         int amount = queryBudget->Calculate(date::year_month_day(date::year{2023}, date::month{4}, date::day{10}),
         date::year_month_day(date::year{2023}, date::month{5},date::day{16}));
@@ -78,6 +115,7 @@ TEST(QueryBudgetTest, calculateBefore) {
 
 TEST(QueryBudgetTest, calculateAfter) {
         StubBudgetRepo* budgetRepo = new StubBudgetRepo();
+        budgetRepo->budgets.push_back(Budget(30, year(2023) / month(9)));
         QueryBudget *queryBudget = new QueryBudget(budgetRepo);
         int amount = queryBudget->Calculate(date::year_month_day(date::year{2025}, date::month{4}, date::day{10}),
         date::year_month_day(date::year{2025}, date::month{5},date::day{16}));
