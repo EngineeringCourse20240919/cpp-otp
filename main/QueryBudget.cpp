31c9c33c4ea8a7d9f644d06fff0c04a8e66b01ab
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2024-09-20 15:46:14 +0800

    :sparkles: start same as  end

--- main/QueryBudget.cpp
+++ main/QueryBudget.cpp
@@ -1,9 +1,12 @@
 #include "QueryBudget.h"
 
-QueryBudget::QueryBudget(BudgetRepo *budgetRepo) {
+QueryBudget::QueryBudget(BudgetRepo *budgetRepo) : budgetRepo(budgetRepo) {
 
 }
 
 unsigned int QueryBudget::Calculate(const year_month_day &start, const year_month_day &end) {
-    return 0;
+    if (budgetRepo->findAll().empty()) {
+        return 0;
+    }
+    return 1;
 }
--- test/QueryBudgetTest.cc
+++ test/QueryBudgetTest.cc
@@ -1,23 +1,48 @@
 #include "gtest/gtest.h"
 #include "../main/QueryBudget.h"
+#include "gmock/gmock.h"
 
 class StubBudgetRepo : public BudgetRepo {
-
+public:
+    MOCK_METHOD(std::vector<Budget>, findAll, ());
 };
+
+using namespace ::testing;
 
 namespace {
 
-    TEST(QueryBudgetTest, NoBudget) {
-        StubBudgetRepo* stubBudgetRepo = new StubBudgetRepo();
-        QueryBudget target = QueryBudget(stubBudgetRepo);
+    class QueryBudgetTest : public Test {
+    protected:
+        StubBudgetRepo *stubBudgetRepo;
+        QueryBudget* target;
+        void SetUp() {
+            stubBudgetRepo = new StubBudgetRepo();
+            target = new QueryBudget(stubBudgetRepo);
+        }
+        void TearDown() {
+            delete stubBudgetRepo;
+            delete target;
+        }
+        void givenBudget(const Budget& budget) {
+            std::vector<Budget> budgets = std::vector<Budget>{ budget };
+            ON_CALL(*stubBudgetRepo, findAll()).WillByDefault(Return(budgets));
+        }
+    };
 
-        int total = target.Calculate(year(2024)/month(9)/day(20), year(2024)/month(9)/day(20));
+    TEST_F(QueryBudgetTest, NoBudget) {
+        int total = target->Calculate(year(2024)/month(9)/day(20), year(2024)/month(9)/day(20));
 
         ASSERT_EQ(0, total);
-
-        delete stubBudgetRepo;
     }
-    
+
+    TEST_F(QueryBudgetTest, StartSameAsEnd) {
+        givenBudget(Budget(30, year(2024)/month(9)));
+
+        int total = target->Calculate(year(2024)/month(9)/day(20), year(2024)/month(9)/day(20));
+
+        ASSERT_EQ(1, total);
+    }
+
 
 
 }
