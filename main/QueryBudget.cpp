242a239608faa253f12b9c51f899ca9357bf4ef4
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2024-09-20 10:08:38 +0800

    :recycle: simplify code for start budget month

--- main/QueryBudget.cpp
+++ main/QueryBudget.cpp
@@ -26,19 +26,17 @@ unsigned int QueryBudget::Calculate(const year_month_day& start, const year_mont
             date::year_month ym_start = startYear / startMonth + months{1};
             date::year_month ym_end = endYear / endMonth - months{1};
             if(ym_start <= ym_end){
-            for (year_month ym = ym_start; ym <= ym_end; ym += months{1}) {
-                std::cout << ym << "\n";
-                if(budget.GetYearMonth() == ym){
+                for (year_month ym = ym_start; ym <= ym_end; ym += months{1}) {
+                    if(budget.GetYearMonth() == ym){
                         allAmount += budget.GetAmount();
                     }
-            }
+                }
             }
             if(budget.GetYearMonth() == year_month(startYear,startMonth)){
-                year_month_day firstDay = start.year() / start.month() / 1;
                 year_month_day lastDay = start.year() / start.month() / last;
-                int startDayNum = (sys_days{start.year() / start.month() / last} - sys_days{start}).count() + 1;
-                int eachDay = (budget.GetAmount() / ((sys_days{lastDay} - sys_days{firstDay}).count() + 1));
-                allAmount += startDayNum * eachDay;
+                int dayNum = (sys_days{start.year() / start.month() / last} - sys_days{start}).count() + 1;
+                int dailyAmount = budget.GetAmount() / (unsigned int) (lastDay.day());
+                allAmount += dayNum * dailyAmount;
             }
             if(budget.GetYearMonth() == year_month(endYear,endMonth)){
                 year_month_day firstDay = end.year() / end.month() / 1;
